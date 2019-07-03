8f7b80b6a587ee02e758cdc050cd461eb4b51162
Callum Whatley <cwhatley@live.ca>
2019-07-02 20:46:06 -0700

    Updates

---
+++ precache-manifest.ac84dafbafd7a4124b3a5e0f1fda824c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b19f461cdbe2feb7d60755a87b590e",
+    "url": "/DineAround/index.html"
+  },
+  {
+    "revision": "7694382add2f91fc4d19",
+    "url": "/DineAround/static/css/main.73d0b477.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/DineAround/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "7694382add2f91fc4d19",
+    "url": "/DineAround/static/js/main.21f18ba2.chunk.js"
+  },
+  {
+    "revision": "ea934b94d5150f5b5208",
+    "url": "/DineAround/static/js/runtime~main.929bd3ad.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/DineAround/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/DineAround/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
